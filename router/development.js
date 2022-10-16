a5b08b14d046a1590d26e1f2086f103be53197ff
ruedaKaf <lapqek.JR@gmail.com>
2022-10-16 11:53:54 -0500

    Arreglos obtener ideas + validacion correo

--- router/development.js
+++ router/development.js
@@ -3,6 +3,7 @@ const development = express.Router();
 const { check } = require('express-validator');
 const { validarCampos } = require('../middlewares/validar-campos');
 const { myValidacion } = require('../models/ideas_tomadas_get');
+const { todasIdeas } = require('../models/todas_ideas_get');
 // Our middleware to verify correct Entity and data
 
 development.use(function timeLog(req, res, next) {
@@ -14,10 +15,13 @@ development.get('/ideasTomadas', async (req, res) => {
   res.json(rta);
   });
 
-development.get('/obtenerIdeas', async (req, res) => {
-  const rta = await obtenerIdea();
-  res.json(rta);
-});
+development.get('/obtenerIdeas',
+[
+check ('email', 'El correo es obligatorio').isEmail(),
+validarCampos
+],
+todasIdeas);
+
 
 development.get('/validacionUsuario',
 [

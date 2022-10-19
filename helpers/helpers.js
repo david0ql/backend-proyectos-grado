6faaf2dee4d3aa2ed9a8b2ccfe5e2bcb5bbf9fc3
ruedaKaf <lapqek.JR@gmail.com>
2022-10-19 15:43:06 -0500

    get,add,delete carrito

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -1,9 +1,16 @@
 const { validacionUsuario } = require('./validacion_usuario');
 const { obtenerIdea } = require('./obtener_idea');
 const { ideaTomada } = require('./idea_tomada');
+const { obtenerCart} = require ('./obtener_cart')
+const { insertCart} = require ('./insert_cart')
+const { deleteCart} = require ('./delete_cart')
 
 module.exports = {
     validacionUsuario,
     obtenerIdea,
-    ideaTomada
+    ideaTomada,
+    obtenerCart,
+    insertCart,
+    deleteCart
+    
 }
